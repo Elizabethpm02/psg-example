799aad8cfe26a3910860b9a18d9a8e4de171975d
Elizabeth Peña <elimaite0219@gmail.com>
2024-05-09 15:29:43 -0500

    Dialogo

---
+++ retos_sesion_03/ejercicio_02.py
@@ -0,0 +1,36 @@
+# Diálogo 
+# Linea 1
+print("Tomás: ¡Hola Rebeca! ¿Cómo estás?")
+ 
+# Linea 2
+print("Rebeca: ¡Hola Tomás! Estoy bien, gracias. ¿Y tú?")
+ 
+# Linea 3
+print("Tomás: Estoy bien también, gracias por preguntar. ¿Has visto qué clima tan agradable tenemos hoy?")
+ 
+# Linea 4
+print("Rebeca: Sí, definitivamente. ¡Es un día precioso! Está bastante agradable. No hace demasiado calor ni frío. ¿Has visto el cielo despejado?")
+
+# Linea 5
+print("Tomás: Sí, es ideal para salir y disfrutar al aire libre.")
+ 
+# Linea 6
+print("Rebeca: ¿Tienes algún plan para hoy?")
+
+# Linea 7
+print("Tomás: Pues estaba pensando en dar un paseo por el parque más tarde. ¿Y tú?")
+
+# Linea 8
+print("Rebeca: Yo voy a almorzar con unos amigos en un restaurante cercano. Será divertido.")
+
+# Linea 9
+print("Tomás: Suena bien. Disfruta tu almuerzo.")
+
+# Linea 10
+print("Rebeca: Gracias,Tomás. Igualmente. Bueno, me tengo que ir. Que tengas un buen día.")
+
+# Linea 11
+print("Tomás: Gracias, Rebeca. Que disfrutes tu almuerzo. ¡Hasta luego!")
+
+# Linea 12
+print("Rebeca: ¡Hasta luego!")
