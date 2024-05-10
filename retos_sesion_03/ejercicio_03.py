ae0f633456a1517488a8553b82636b158b52e09e
Elizabeth Peña <elimaite0219@gmail.com>
2024-05-09 20:01:51 -0500

    Tutorial de como barrer una habitacion

---
+++ retos_sesion_03/ejercicio_03.py
@@ -0,0 +1,45 @@
+print ("**TUTORIAL DE COMO BARRER UNA HABITACIÓN**")
+
+print ("MATERIALES NECESARIOS:")
+
+print ("*Escoba con cerdas firmes, *Recogedor, *Bolsa de basura")
+
+print ("PASOS PARA BARRER UNA HABITACIÓN:")
+
+print ("1. PREPARACIÓN:")
+
+print ("Reúne todos los materiales necesarios: la escoba, el recogedor y una bolsa de basura.")
+
+print ("Antes de barrer, se debe recoger todos los objetos del suelo (juguetes, papeles, etc.), y colocarlos en su lugar designado.")
+
+print ("2. AGARRAR LA ESCOBA:")
+
+print ("Toma la escoba con ambas manos para sostenerla firmemente y tener un mejor control al momento de barrer.")
+
+print ("3. COMIENZA DESDE EL FONDO DE LA HABITACIÓN:")
+
+print ("Empieza a barrer desde el fondo de la habitación hacia la salida de esta, esto permitirá barrer ordenadamente para evitar pisar las áreas ya limpiadas.")
+
+print ("4. BARRIDO:")
+
+print ("Con la escoba, Asegúrate de barrer debajo de los muebles y esquinas en donde se acumula el polvo.")
+
+print ("Utiliza movimientos firmes al momento de barrer y empuja la suciedad y el polvo hacia una esquina o centro de la habitación.")
+
+print ("5. RECOGER LA SUCIEDAD:")
+
+print ("Una vez que hayas barrido toda la habitación y reunir toda la suciedad en un área específica. Coloca el borde del recogedor cerca de la suciedad en el suelo, con la escoba pasa la suciedad del suelo hacia el recogedor.")
+
+print ("Una vez la suciedad este dentro del recogedor, se debe llevar hacia una bolsa de basura. Con cuidado, inclina el recogedor para verter la suciedad en la bolsa de basura.")
+
+print ("6. REVISION FINAL:")
+
+print ("Después de barrer toda la habitación, realiza una revisión final para asegurar que no hayas dejado ninguna suciedad detrás.")
+
+print ("7. ELIMINAR LA BASURA:")
+
+print ("Terminado la limpieza, se debe depositar la bolsa en el contenedor de basura adecuado.")
+
+print ("8. LIMPIAR LA ESCOBA Y EL RECOGEDOR:")
+
+print ("Una vez terminado, Asegúrate de limpiar la escoba y el recogedor para mantenerlos en buen estado.")
