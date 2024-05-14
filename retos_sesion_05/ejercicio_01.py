35b9e4de21e2b17772e2a7f19b619fb443f1e3fe
Elizabeth Peña <elimaite0219@gmail.com>
2024-05-14 18:41:34 -0500

    Salario

---
+++ retos_sesion_05/ejercicio_01.py
@@ -0,0 +1,12 @@
+# Definir variables
+horas_trabajadas = 160
+tarifas_x_hora = 5.5
+
+# Calcular el salario
+salario = horas_trabajadas * tarifas_x_hora
+
+# Imprimir el resultado
+print("El salario es:")
+print(salario)
+
+
