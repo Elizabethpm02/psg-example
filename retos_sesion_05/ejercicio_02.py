e872e3f8f5704aeeafb1c8cf0fec78baf25976ca
Elizabeth Peña <elimaite0219@gmail.com>
2024-05-15 23:52:32 -0500

    Temperatura

---
+++ retos_sesion_05/ejercicio_02.py
@@ -0,0 +1,14 @@
+# Definir variables
+temperatura_1 = 30
+temperatura_2 = -273.99
+temperatura_3 = 100
+
+# Conversion de temperaturas de grados Celsius a grados Fahrenheit
+C1 = (temperatura_1 * 9/5) + 32
+C2 = (temperatura_2 * 9/5) + 32
+C3 = (temperatura_3 * 9/5) + 32
+
+# Imprimir el resultado
+print(C1)
+print(C2)
+print(C3)
