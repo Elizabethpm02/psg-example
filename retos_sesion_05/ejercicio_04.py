e7384c0f6a23ff80936794366b303d5552553ff5
Elizabeth Peña <elimaite0219@gmail.com>
2024-06-09 17:02:04 -0500

    cubo

---
+++ retos_sesion_05/ejercicio_04.py
@@ -0,0 +1,8 @@
+# Longitud del lado del cubo
+lado = 10 
+
+# Calculo del volumen del cubo
+volumen_cubo = lado ** 3
+
+# Imprimir resultado
+print("El volumen del cubo es:", volumen_cubo, "metros cúbicos")
